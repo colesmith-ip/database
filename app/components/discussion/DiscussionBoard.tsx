71c778f9b82b1a0af7a20199bc9a3f8125261003
Cole Smith <colesmith@Coles-MacBook-Pro.local>
2025-08-15 10:25:05 -0400

    Add dynamic export to DiscussionBoard component to fix final build issue

--- app/components/discussion/DiscussionBoard.tsx
+++ app/components/discussion/DiscussionBoard.tsx
@@ -2,6 +2,9 @@ import { getPosts } from '../../actions/discussion'
 import CreatePostForm from './CreatePostForm'
 import PostCard from './PostCard'
 
+// Force dynamic rendering to prevent build-time database calls
+export const dynamic = 'force-dynamic'
+
 export default async function DiscussionBoard() {
   const posts = await getPosts()
 
